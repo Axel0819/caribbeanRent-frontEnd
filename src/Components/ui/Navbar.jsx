6b72bf01d69432a70726b2e5df98459609f710ca
Sofia Sanchez <sofisn2699@gmail.com>
2022-07-01 00:40:57 -0600

    Avance navbar: logo, display y border color

--- src/Components/ui/Navbar.jsx
+++ src/Components/ui/Navbar.jsx
@@ -1,21 +1,26 @@
 import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { MainLayoutContext } from '../../Services/Context/MainLayoutContext';
+import { Container } from '@mui/material';
+import logo from '../../Assets/Images/logo.png';
 
 export const Navbar = () => {
     const { handleOpenModal } = useContext(MainLayoutContext)
     return (
-        <nav style={{gap: '50px'}} className="flex">
-            <div>Navbar</div>
-            <ul>
-                <li>
-                    <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : ''}>
-                        Home
-                    </NavLink>
-                </li>
-            </ul>
+        <nav className="navbar-border-bottom py-3">
+            <Container>
+                <div className="flex justify-content-between">
+                    <img src={logo} alt="Logo Caribbean Rent" />
 
-            <button onClick={ handleOpenModal }>Abrir modal</button>
+                    <div className="flex gap-20">
+                        <button>Buscar</button>
+                        <button>Menu</button>
+                    </div>
+                </div>
+
+            </Container>
+
+            {/* <button onClick={ handleOpenModal }>Abrir modal</button> */}
         </nav>
     )
 }
