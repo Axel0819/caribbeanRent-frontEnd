ddb7ea176e9206ad859e4fc9b293fe42d7e13c64
Sofia Sanchez <sofisn2699@gmail.com>
2022-07-02 01:22:21 -0600

    Tarea finalizada. Close #8

--- src/Components/ui/Navbar.jsx
+++ src/Components/ui/Navbar.jsx
@@ -3,6 +3,13 @@ import { NavLink } from 'react-router-dom';
 import { MainLayoutContext } from '../../Services/Context/MainLayoutContext';
 import { Container } from '@mui/material';
 import logo from '../../Assets/Images/logo.png';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
+
 
 export const Navbar = () => {
     const { handleOpenModal } = useContext(MainLayoutContext)
@@ -12,9 +19,16 @@ export const Navbar = () => {
                 <div className="flex justify-content-between">
                     <img src={logo} alt="Logo Caribbean Rent" />
 
-                    <div className="flex gap-20">
-                        <button>Buscar</button>
-                        <button>Menu</button>
+                    <div className="flex gap-20 align-items-center">
+
+                        <Fab size="large" saria-label="">
+                            <i class="fa fa-solid fa-magnifying-glass"></i>
+                        </Fab>
+
+                        <Fab variant="extended" size="large" aria-label="">
+                            <i class="fa fa-solid fa-list pr-3"></i>
+                            <i class="fa fa-solid fa-user color-icon"></i>
+                        </Fab>
                     </div>
                 </div>
 
