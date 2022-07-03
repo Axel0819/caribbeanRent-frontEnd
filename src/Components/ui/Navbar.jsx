db18661d0791f154400723021d39d27ba1daec04
Rafael Sequeira Sandoval <rafaels@grupomundial.cr>
2022-07-02 20:23:57 -0600

    Menu agregrado al navbar. Close #19

--- src/Components/ui/Navbar.jsx
+++ src/Components/ui/Navbar.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import { Container, Fab } from '@mui/material';
 import logo from '../../Assets/Images/logo.png';
+import { NavbarMenu } from './NavbarMenu';
 
 export const Navbar = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleOpenMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <nav className="navbar-border-bottom py-3">
             <Container>
@@ -14,10 +25,25 @@ export const Navbar = () => {
                             <i className="fa fa-solid fa-magnifying-glass"></i>
                         </Fab>
 
-                        <Fab variant="extended" size="medium" className="py-4">
+                        <Fab
+                            variant="extended"
+                            size="medium"
+                            className="py-4"
+                            id="basic-button"
+                            aria-controls={open ? 'account-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            onClick={handleOpenMenu}
+                        >
                             <i className="fa fa-solid fa-list pr-3"></i>
                             <i className="fa fa-solid fa-user color-icon"></i>
                         </Fab>
+                        <NavbarMenu 
+                            anchorEl={ anchorEl }
+                            handleClose={ handleClose }
+                            open={ open }
+                        />
+                        
                     </div>
                 </div>
 
