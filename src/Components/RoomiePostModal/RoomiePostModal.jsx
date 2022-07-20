a2318d3c65b89405fdd56e574ca44db8b697e9b1
Rafael Sequeira Sandoval <rafaels@grupomundial.cr>
2022-07-19 23:52:03 -0600

    cambios realizados

---
+++ src/Components/RoomiePostModal/RoomiePostModal.jsx
@@ -0,0 +1,16 @@
+export const RoomiePostModal = () => {
+  return (
+    <div style={{ width: '500px', backgroundColor: 'red' }}>
+        <h1>RoomiePostModal</h1>
+        <h1>RoomiePostModal</h1>
+        <h1>RoomiePostModal</h1>
+        <h1>RoomiePostModal</h1>
+        <h1>RoomiePostModal</h1>
+        <h1>RoomiePostModal</h1>
+        <h1>RoomiePostModal</h1>
+        <h1>RoomiePostModal</h1>
+        <h1>RoomiePostModal</h1>
+        <h1>RoomiePostModal</h1>
+    </div>
+  )
+}
